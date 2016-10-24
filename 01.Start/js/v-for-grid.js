5a239858e230e50e7f39bf2fa053dc7b1546eef8
tzdk <yefeng_dk@163.com>
2016-10-24 14:55:42 +0800

    vue

---
+++ 01.Start/js/v-for-grid.js
@@ -0,0 +1,26 @@
+window.onload=function(){ 
+  var vm = new Vue({
+			el: '#app',
+			data: {
+				people: [{
+					name: 'Jack',
+					age: 30,
+					sex: 'Male'
+				}, {
+					name: 'Bill',
+					age: 26,
+					sex: 'Male'
+				}, {
+					name: 'Tracy',
+					age: 22,
+					sex: 'Female'
+				}, {
+					name: 'Chris',
+					age: 36,
+					sex: 'Male'
+				}]
+			}
+	})
+} 
+        
+
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ### 常用指令
 - [v-bind指令示例](https://tzdk.github.io/VueDemo/01.Start/v-bind.html)
 - [v-for指令示例](https://tzdk.github.io/VueDemo/01.Start/v-for.html)
+- [v-for-grid指令示例](https://tzdk.github.io/VueDemo/01.Start/v-for.html)，html文件单独引用js文件。
